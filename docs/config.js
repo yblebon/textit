d93dcddc57af32648e00d9c77a035667f4e08b6e
yasser <ysr.b.lebon@gmail.com>
2025-11-15 12:33:41 +0100

    update

---
+++ docs/config.js
@@ -0,0 +1,12 @@
+// config.js
+// Keep this file private or .gitignore it if using PAT
+window.APP_CONFIG = {
+  // GitHub repository in owner/repo format
+  repo: "yblebon/hooks",
+
+  // Personal Access Token with 'repo' and 'workflow' scopes
+  token: "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",
+
+  // Workflow filename (without .yml)
+  workflow: "encrypt-text"
+};
